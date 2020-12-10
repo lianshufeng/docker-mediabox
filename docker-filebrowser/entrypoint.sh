6a9591b7830023d301ba30671cab2b80fdfc296c
lianshufeng <251708339@qq.com>
2020-12-10 11:39:36 +0800

    fix bug : filebrowser share update filebrowser version

--- docker-filebrowser/entrypoint.sh
+++ docker-filebrowser/entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-#如果
-ls /tmp/backup/
-
-
-
-if [ ! -f "/db/database.db" ];then
- cp /tmp/backup/database.db /db/database.db
- echo "copy database.db"
-fi
-
-
 #启动服务
 /filebrowser
---
+++ docker-filebrowser/pullFileBrowserdb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+#更新db
+docker run -d --name filebrowser filebrowser/filebrowser:v2.10.0
+docker cp filebrowser:/.filebrowser.json .filebrowser.json
+#docker cp filebrowser:/database.db database.db #不需要拷贝,不存在会自动新建
+docker rm -f filebrowser
+
+#更新路径
+sed -i "s/\/database.db/\/db\/database.db/g" .filebrowser.json
