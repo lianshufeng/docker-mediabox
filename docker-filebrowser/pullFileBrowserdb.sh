932a3fe10fb83021dc4417b4bcaf45c7004f3af9
lianshufeng <251708339@qq.com>
2022-09-19 11:02:50 +0800

    update

--- docker-filebrowser/pullFileBrowserdb.sh
+++ docker-filebrowser/pullFileBrowserdb.sh
@@ -2,7 +2,7 @@
 
 
 #更新db
-docker run -d --name filebrowser filebrowser/filebrowser:v2.10.0
+docker run -d --name filebrowser filebrowser/filebrowser:v2.22.4
 docker cp filebrowser:/.filebrowser.json .filebrowser.json
 #docker cp filebrowser:/database.db database.db #不需要拷贝,不存在会自动新建
 docker rm -f filebrowser
